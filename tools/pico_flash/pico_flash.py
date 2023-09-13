6318c8eedeccf54b9e91818768f66054bd080958
Jeff Curless <jeff@thecurlesses.com>
2023-09-13 17:26:51 -0400

    Updated to be a shell command.
    Modified the script to that we include the /usr/bin/python3 at the
    begining so users do not have to remember to run the command with
    "python3" first.  Also updated the help messages and error processing a
    little so that if theere is no firmware file mentioned, the program
    shuts down nicely.

--- tools/pico_flash/pico_flash.py
+++ tools/pico_flash/pico_flash.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 import time
 import shutil
@@ -6,12 +7,12 @@ import argparse
 #
 # Setup arguments so we can determine where the version if PICO firmware we want is...
 #
-parser = argparse.ArgumentParser( description='PICO Flasher' )
-parser.add_argument( 'uf2_file_path',  metavar="UF2_FILE", type=str, help='Path to the UF2 file')
+parser = argparse.ArgumentParser( prog='pico_flash', description="Program to update firmware on multiple PICO's." )
+parser.add_argument( '-d', '--uf2', help='Path to the firmware file in UF2 format')
+
 
 args = parser.parse_args()
-
-uf2_file_path = args.uf2_file_path
+uf2_file_path = args.uf2
 
 def find_pico_device():
     """
@@ -34,22 +35,27 @@ def find_pico_device():
 
     return output.strip()
 
-print( "Press Ctrl-C to exit - DO NOT TERMINATE while transfer is in progress!")
+if uf2_file_path is None:
+    print( "\n\nError: Missing firmware file to flash to PICO's.\n\n")
+    parser.parse_args(['-h'])
+else:
+    print( "Press Ctrl-C to exit - DO NOT TERMINATE while transfer is in progress!")
 
-while True:
-    device = find_pico_device()
-    if os.path.exists( device ):
-        print( 'PICO detected on ' + device )
-        try:
-            print( '    Writing firmware do not terminate!' )
-            shutil.copy2(uf2_file_path, device )
-            print( '    Firmware installed - Rebooting PICO' )
-            time.sleep(2)
-        except PermissionError:
-            print( '    Permission Error:  Unable to copy firmware to PICO.  Please reconnect the PICO and try again.' )
-        except FileNotFoundError:
-            print( '    Error: UF2 file not found.  Please check the file location.' )
-    else:
-        print( 'Searching for PICO...' )
+    while True:
+        device = find_pico_device()
+        if os.path.exists( device ):
+            print( 'PICO detected on ' + device )
+            try:
+                print( '    Writing firmware do not terminate!' )
+                shutil.copy2(uf2_file_path, device )
+                print( '    Firmware installed - Rebooting PICO' )
+                time.sleep(2)
+            except PermissionError:
+                print( '    Permission Error:  Unable to copy firmware to PICO.  Please reconnect the PICO and try again.' )
+            except FileNotFoundError:
+                print( '    Error: UF2 file not found.  Please check the file location.' )
+        else:
+            print( 'Searching for PICO...' )
 
-    time.sleep(5)
+        time.sleep(5)
+
